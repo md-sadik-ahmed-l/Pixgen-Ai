f1dbf0f3d6ac40622693344092d7287aadffbf65
sadik111222 <md8413226@gmail.com>
2026-04-28 10:59:52 +0600

    added top photos card section

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,24 +1,30 @@
 import PhotoCard from "@/components/PhotoCard";
+import Link from "next/link";
+import { BiArrowBack, BiArrowFromRight } from "react-icons/bi";
+import { FaArrowLeft } from "react-icons/fa";
 
 const HomePage = async () => {
-  const res = await fetch("https://pixgen-ai.vercel.app/data.json");
+  const res = await fetch("https://pixgen-eta.vercel.app/data.json");
   const photos = await res.json();
   const topPhotos = photos.slice(0, 8);
 
   return (
     <div>
 
-      <h1>Top Generations</h1>
+      <h1 className="font-bold text-4xl mt-10 mb-5">Top Generations</h1>
 
-      <div>
+      <div className="grid grid-cols-4 gap-5">
 
         {topPhotos.map((photo) => (
 
           <PhotoCard key={photo.id} photo={photo}></PhotoCard>
 
         ))}
+        
       </div>
-      
+
+      <div className="flex justify-center items-center "><Link href={"/all-photos"} className="border mt-10 text-xl font-medium py-3 px-9 rounded-full shadow-md bg-linear-to-r from-pink-500 via-purple-500 bg-red-500 text-white">All Photos</Link></div>
+
     </div>
   );
 };
--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import { Button, Card, Chip, Separator } from "@heroui/react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { FaHeart } from "react-icons/fa";
+import { BiDownload } from "react-icons/bi";
 
-const PhotoCard = ({photo}) => {
-    return (
-        <div>
-            <h1>{photo.title}</h1>
+const PhotoCard = ({ photo }) => {
+  return (
+    <div>
+      <Card className="border rounded-xl">
+        <div className="relative w-full aspect-square">
+          <Image
+            src={photo.imageUrl}
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            alt={photo.title}
+            className="object-cover rounded-xl"
+          />
+
+          <Chip size="sm" className="absolute right-2 top-2">
+            {photo.category}
+          </Chip>
         </div>
-    );
+
+        <div>
+          <h2 className="font-medium text-xl">{photo.title}</h2>
+        </div>
+
+        <div className="flex gap-5">
+          <div className="flex items-center gap-2">
+            <p>
+              <FaHeart />
+            </p>
+            <p>{photo.likes}</p>
+          </div>
+
+          <Separator orientation="vertical" />
+
+          <div className="flex items-center gap-2">
+
+            <p>
+              <BiDownload />
+            </p>
+
+            <p>{photo.downloads}</p>
+          </div>
+        </div>
+
+        <Link href={`/all-photos/${photo.id}`} className="py-2 ">
+          {" "}
+          <Button variant="outline" className="w-full text-xl shadow-sm">
+            View
+          </Button>
+        </Link>
+      </Card>
+      
+    </div>
+  );
 };
 
 export default PhotoCard;
